a91456e4df3e8b1a60d7eeb54811ffac66371ff4
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-05-28 12:39:02 +0900

    Feat: implement Firebase Google login successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName ="1.0.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -41,57 +41,58 @@ android {
                 "proguard-rules.pro"
             )
         }
+
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+
+        buildFeatures {
+            compose = true
+            buildConfig = true
+        }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+    dependencies {
+        implementation("com.airbnb.android:lottie-compose:6.1.0")
+        implementation(project(":core-ui"))
+        implementation(project(":feature"))
+        implementation(libs.androidx.navigation.compose)
+
+        val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+        implementation(composeBom)
+        androidTestImplementation(composeBom)
+
+        implementation("androidx.compose.material3:material3")
+        implementation("androidx.compose.material:material")
+        implementation("androidx.compose.foundation:foundation")
+        implementation("androidx.compose.ui:ui")
+        implementation("androidx.compose.ui:ui-tooling-preview")
+        debugImplementation("androidx.compose.ui:ui-tooling")
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+        debugImplementation("androidx.compose.ui:ui-test-manifest")
+        implementation("androidx.compose.material:material-icons-core")
+        implementation("androidx.compose.material:material-icons-extended")
+        implementation("androidx.compose.material3.adaptive:adaptive")
+        implementation("androidx.activity:activity-compose:1.10.0")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+        implementation("androidx.compose.runtime:runtime-livedata")
+        implementation("androidx.compose.runtime:runtime-rxjava2")
+
+        implementation(libs.hilt.android)
+        ksp(libs.hilt.android.compiler)
+
+        implementation(libs.androidx.appcompat)
+        implementation(libs.play.services.oss.licenses)
+
+        implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+        implementation("com.google.firebase:firebase-analytics")
+        implementation(libs.firebase.auth)
+        implementation(libs.play.services.auth)
     }
-
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
-}
-
-dependencies {
-    implementation("com.airbnb.android:lottie-compose:6.1.0")
-    implementation(project(":core-ui"))
-    implementation(project(":feature"))
-    implementation(libs.androidx.navigation.compose)
-
-    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material")
-    implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.compose.material:material-icons-core")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.material3.adaptive:adaptive")
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.compose.runtime:runtime-rxjava2")
-
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
-
-    implementation(libs.androidx.appcompat)
-    implementation(libs.play.services.oss.licenses)
-
-    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation(libs.firebase.auth)
-    implementation(libs.play.services.auth)
 }
