434a3c9ec3e6c3f32363217d078f35e7894cd58b
starshape77 <111961655+starshape7@users.noreply.github.com>
2025-06-24 19:51:29 +0900

    FIX: WebFlux url 추가

--- core-data/src/main/java/com/example/core/data/network/RetrofitInstance.kt
+++ core-data/src/main/java/com/example/core/data/network/RetrofitInstance.kt
@@ -3,6 +3,7 @@ package com.example.core.data.network
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import com.example.core.data.api.AIChatService
 import com.example.core.data.api.AuthService
 import com.example.core.data.api.UserService
 import okhttp3.OkHttpClient
@@ -12,6 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
 
     private const val BASE_URL = "https://www.gotoend.store/mvc/"
+    private const val BASE_URL_WEBFLUX = "https://www.gotoend.store/webflux/"
     private lateinit var prefs: SharedPreferences
 
     fun init(context: Context) {
@@ -31,12 +33,24 @@ object RetrofitInstance {
         .addInterceptor(AuthInterceptor())
         .build()
 
+    // MVC용 Retrofit
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    // WebFlux용 Retrofit
+    private val retrofitWebflux = Retrofit.Builder()
+        .baseUrl(BASE_URL_WEBFLUX)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    //MVC용 서비스
     val authService: AuthService = retrofit.create(AuthService::class.java)
     val userService: UserService = retrofit.create(UserService::class.java)
+
+    //WebFlux용 서비스
+    val aiChatService : AIChatService = retrofitWebflux.create(AIChatService::class.java)
 }
